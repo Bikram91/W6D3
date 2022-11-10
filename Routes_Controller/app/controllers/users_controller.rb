431c3bc8f09d590fd80cc6e27c91b577b0b81037
Saleh Alkaheli <skalkaheli@gmail.com>
2022-11-09 16:54:24 -0800

    Phase 3 finished

--- Routes_Controller/app/controllers/users_controller.rb
+++ Routes_Controller/app/controllers/users_controller.rb
@@ -1,3 +1,22 @@
 class UsersController < ApplicationController
-    
+
+
+    def index
+        # users = User.all
+        # render json: users
+        # render plain: "I'm in the index action!"
+        render json: params
+
+    end
+
+    def create
+        render json: params
+    end
+
+    def show
+        render json: params
+    end
+
+
+
 end
